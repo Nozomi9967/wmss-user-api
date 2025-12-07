835b87b8ab45cbcec503d7834cdeb79e1a884c91
Nozomi <q1948875496@outlook.com>
2025-12-07 22:44:57 +0800

    修复bug

--- internal/logic/user/updateuserlogic.go
+++ internal/logic/user/updateuserlogic.go
@@ -8,6 +8,7 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/Nozomi9967/wmss-user-api/common"
 	"github.com/Nozomi9967/wmss-user-api/internal/model"
 	"github.com/Nozomi9967/wmss-user-api/internal/svc"
 	"github.com/Nozomi9967/wmss-user-api/internal/types"
@@ -30,12 +31,28 @@ func NewUpdateUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Update
 }
 
 func (l *UpdateUserLogic) UpdateUser(req *types.UpdateUserReq) (resp *types.Response, err error) {
-	// 1. 从 Context 中获取要操作的用户ID（假设用户只能更新自己的信息）
 	userId := l.ctx.Value("user_id").(string)
+
+	var currentUser *types.UserInfo
+	currentUser, err = l.svcCtx.SysUserModel.SelectOneDetail(l.ctx, userId)
+	if err != nil {
+		l.Logger.Errorf("查询用户失败: %v", err)
+		return &types.Response{
+			Code: 500,
+			Msg:  "查询用户失败",
+		}, nil
+	}
+	if currentUser.RoleID != common.SUPER_ADMIN_ROLE_ID {
+		l.Logger.Errorf("查询用户失败，权限不足: %v", err)
+		return &types.Response{
+			Code: 500,
+			Msg:  "查询用户失败，权限不足",
+		}, nil
+	}
 
 	// 2. 查找现有用户记录
 	var user *model.SysUser
-	user, err = l.svcCtx.SysUserModel.FindOne(l.ctx, userId)
+	user, err = l.svcCtx.SysUserModel.FindOne(l.ctx, req.UserID)
 	if err != nil {
 		// 统一处理查找失败，可能是 ErrNotFound 或数据库错误
 		l.Logger.Errorf("查找用户失败，无法更新: %v", err)
@@ -74,8 +91,8 @@ func (l *UpdateUserLogic) UpdateUser(req *types.UpdateUserReq) (resp *types.Resp
 
 	// 日期字段：PasswordExpireTime (需要从 string 转换为 time.Time)
 	if req.PasswordExpireTime != "" {
-		// 假设日期格式为 "YYYY-MM-DD"
-		const layout = "2006-01-02"
+		// 日期格式为 "yyyy-MM-dd HH:mm:ss"
+		const layout = "2006-01-02 15:04:05"
 
 		// 使用 time.Local 解析，以确保时区设置正确
 		parsedTime, parseErr := time.ParseInLocation(layout, req.PasswordExpireTime, time.Local)
@@ -83,7 +100,7 @@ func (l *UpdateUserLogic) UpdateUser(req *types.UpdateUserReq) (resp *types.Resp
 			l.Logger.Errorf("更新用户失败: 密码过期时间格式错误: %v", parseErr)
 			return &types.Response{
 				Code: 400, // 400 Bad Request
-				Msg:  "密码过期时间格式错误，请使用 YYYY-MM-DD 格式",
+				Msg:  "密码过期时间格式错误，请使用 yyyy-MM-dd HH:mm:ss 格式",
 			}, nil
 		}
 		user.PasswordExpireTime = parsedTime
