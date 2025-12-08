5b4fc4f87345cde4c06bbce01f0e7ec41d2b18d6
Nozomi <q1948875496@outlook.com>
2025-12-08 20:27:26 +0800

    分配角色权限

--- internal/logic/role/assignrolepermissionslogic.go
+++ internal/logic/role/assignrolepermissionslogic.go
@@ -5,6 +5,7 @@ package role
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Nozomi9967/wmss-user-api/common"
 	"github.com/Nozomi9967/wmss-user-api/internal/model"
@@ -29,7 +30,6 @@ func NewAssignRolePermissionsLogic(ctx context.Context, svcCtx *svc.ServiceConte
 }
 
 func (l *AssignRolePermissionsLogic) AssignRolePermissions(req *types.AssignRolePermissionsReq) (resp *types.Response, err error) {
-	// todo: add your logic here and delete this line
 	userId, ok := l.ctx.Value("user_id").(string)
 	if !ok || userId == "" {
 		l.Logger.Errorf("登录过期：用户ID为空或类型错误")
@@ -55,23 +55,75 @@ func (l *AssignRolePermissionsLogic) AssignRolePermissions(req *types.AssignRole
 			Msg:  "权限不足，仅超级管理员可更新角色",
 		}, nil
 	}
-	var data []*model.SysRolePermission
-	for _, permissionId := range req.PermissionIDs {
-		var permission model.SysRolePermission
-		permission.PermissionId = permissionId
-		permission.RoleId = req.RoleID
-		data = append(data, &permission)
-	}
-	_, err = l.svcCtx.SysRolePermissionModel.InsertBatch(l.ctx, data)
+
+	// 1. 查询该角色现有的所有权限
+	existingPermissionIds, err := l.svcCtx.SysRolePermissionModel.FindPermissionsByRoleId(l.ctx, req.RoleID)
 	if err != nil {
-		l.Logger.Errorf("分配权限失败")
+		l.Logger.Errorf("查询角色[%s]现有权限失败: %v", req.RoleID, err)
 		return &types.Response{
 			Code: 500,
-			Msg:  "分配权限失败",
+			Msg:  "查询现有权限失败",
 		}, nil
 	}
+
+	// 2. 将请求的权限ID转为 map，方便查找
+	newPermissionMap := make(map[string]bool)
+	for _, permissionId := range req.PermissionIDs {
+		newPermissionMap[permissionId] = true
+	}
+
+	// 3. 找出需要删除的权限（存在于数据库但不在新列表中）
+	var toDelete []string
+	for _, existingId := range existingPermissionIds {
+		if !newPermissionMap[existingId] {
+			toDelete = append(toDelete, existingId)
+		}
+	}
+
+	// 4. 找出需要新增的权限（在新列表中但不在数据库中）
+	existingPermissionMap := make(map[string]bool)
+	for _, existingId := range existingPermissionIds {
+		existingPermissionMap[existingId] = true
+	}
+
+	var toInsert []*model.SysRolePermission
+	for _, permissionId := range req.PermissionIDs {
+		if !existingPermissionMap[permissionId] {
+			toInsert = append(toInsert, &model.SysRolePermission{
+				RoleId:       req.RoleID,
+				PermissionId: permissionId,
+			})
+		}
+	}
+
+	// 5. 执行删除操作
+	if len(toDelete) > 0 {
+		err = l.svcCtx.SysRolePermissionModel.DeleteByRoleIdAndPermissionIds(l.ctx, req.RoleID, toDelete)
+		if err != nil {
+			l.Logger.Errorf("删除角色[%s]的权限失败: %v", req.RoleID, err)
+			return &types.Response{
+				Code: 500,
+				Msg:  "删除权限失败",
+			}, nil
+		}
+		l.Logger.Infof("删除角色[%s]的 %d 个权限", req.RoleID, len(toDelete))
+	}
+
+	// 6. 执行新增操作
+	if len(toInsert) > 0 {
+		_, err = l.svcCtx.SysRolePermissionModel.InsertBatch(l.ctx, toInsert)
+		if err != nil {
+			l.Logger.Errorf("新增角色[%s]的权限失败: %v", req.RoleID, err)
+			return &types.Response{
+				Code: 500,
+				Msg:  "新增权限失败",
+			}, nil
+		}
+		l.Logger.Infof("新增角色[%s]的 %d 个权限", req.RoleID, len(toInsert))
+	}
+
 	return &types.Response{
 		Code: 200,
-		Msg:  "分配权限成功",
-	}, err
+		Msg:  fmt.Sprintf("分配权限成功，删除 %d 个，新增 %d 个", len(toDelete), len(toInsert)),
+	}, nil
 }
--- internal/model/sysRolePermissionModel.go
+++ internal/model/sysRolePermissionModel.go
@@ -1,6 +1,12 @@
 package model
 
-import "github.com/zeromicro/go-zero/core/stores/sqlx"
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 var _ SysRolePermissionModel = (*customSysRolePermissionModel)(nil)
 
@@ -10,6 +16,7 @@ type (
 	SysRolePermissionModel interface {
 		sysRolePermissionModel
 		withSession(session sqlx.Session) SysRolePermissionModel
+		DeleteByRoleIdAndPermissionIds(ctx context.Context, roleId string, permissionIds []string) error
 	}
 
 	customSysRolePermissionModel struct {
@@ -26,4 +33,28 @@ func NewSysRolePermissionModel(conn sqlx.SqlConn) SysRolePermissionModel {
 
 func (m *customSysRolePermissionModel) withSession(session sqlx.Session) SysRolePermissionModel {
 	return NewSysRolePermissionModel(sqlx.NewSqlConnFromSession(session))
+}
+
+func (m *defaultSysRolePermissionModel) DeleteByRoleIdAndPermissionIds(ctx context.Context, roleId string, permissionIds []string) error {
+	if len(permissionIds) == 0 {
+		return nil
+	}
+
+	placeholders := make([]string, len(permissionIds))
+	args := make([]interface{}, len(permissionIds)+1)
+	args[0] = roleId
+
+	for i, id := range permissionIds {
+		placeholders[i] = "?"
+		args[i+1] = id
+	}
+	placeholderStr := strings.Join(placeholders, ",")
+
+	query := fmt.Sprintf(
+		"delete from %s where `role_id` = ? and `permission_id` in (%s)",
+		m.table, placeholderStr,
+	)
+
+	_, err := m.conn.ExecCtx(ctx, query, args...)
+	return err
 }
